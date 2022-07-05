edc3e52e97cc682bfada2d9f82fed2788c520d26
Aryan Ahadinia <62179268+AryanAhadinia@users.noreply.github.com>
2022-07-05 21:28:12 +0430

    Declare file encoding

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,62 +3,38 @@ package main;
 import db.sql.PostgresConnection;
 import db.sql.PostgresManager;
 import db.timeseries.InfluxClient;
-import org.apache.commons.cli.*;
 import server.Server;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Main {
+    private static final String DB_PROPERTIES = "db.properties";
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        Options options = new Options();
-        options.addOption(Option.builder("psql_url")
-                        .desc("Postgres token")
-                        .hasArg()
-                        .required()
-                        .build())
-                .addOption(Option.builder("psql_user")
-                        .desc("Postgres username")
-                        .hasArg()
-                        .required()
-                        .build())
-                .addOption(Option.builder("psql_pass")
-                        .desc("Postgres password")
-                        .hasArg()
-                        .required()
-                        .build())
-                .addOption(Option.builder("influx_url")
-                        .desc("InfluxDB url")
-                        .hasArg()
-                        .required()
-                        .build())
-                .addOption(Option.builder("influx_token")
-                        .desc("InfluxDB token")
-                        .hasArg()
-                        .required()
-                        .build());
-        try {
-            CommandLine commandLine = new DefaultParser().parse(options, args);
-            String postgresURL = commandLine.getOptionValue("psql_url");
-            String postgresUsername = commandLine.getOptionValue("psql_user");
-            String postgresPassword = commandLine.getOptionValue("psql_pass");
-            PostgresConnection.createInstance(postgresURL, postgresUsername, postgresPassword);
-            PostgresManager.createTables();
-            String influxURL = commandLine.getOptionValue("influx_url");
-            String influxToken = commandLine.getOptionValue("influx_token");
-            InfluxClient.createInstance(influxURL, influxToken);
+        System.out.println("Server initialized.");
+        try (InputStream propertiesStream = ClassLoader.getSystemResourceAsStream(DB_PROPERTIES)) {
+            Properties properties = new Properties();
+            properties.load(propertiesStream);
+
+            String postgresURL = properties.getProperty("PSQL_URL");
+            String postgresUsername = properties.getProperty("PSQL_USER");
+            String postgresPassword = properties.getProperty("PSQL_PASS");
+            String influxURL = properties.getProperty("INFLUX_URL");
+            String influxToken = properties.getProperty("INFLUX_TOKEN");
+
+//            PostgresConnection.createInstance(postgresURL, postgresUsername, postgresPassword);
+//            InfluxClient.createInstance(influxURL, influxToken);
+//            PostgresManager.createTables();
+
             Server server = Server.getInstance(8080);
             server.start();
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
-            e.printStackTrace();
-            System.exit(1);
-        } catch (ParseException e) {
-            System.err.println(e.getMessage());
-            new HelpFormatter().printHelp("Required options missed", options);
-            e.printStackTrace();
-            System.exit(1);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
         }
     }
 }
